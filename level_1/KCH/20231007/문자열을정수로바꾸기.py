b7d3d4a92f7269ea148aead8bf7d65abc5184c38
김채현 <kch@gimchaehyeon-ui-MacBookAir.local>
2023-10-07 13:51:45 +0900

    feat: 레벨1 - 20231007 - 김채현

---
+++ level_1/KCH/20231007/문자열을정수로바꾸기.py
@@ -0,0 +1,3 @@
+# 예상 시간복잡도: O(logN), N: 문자열의 길이
+def solution(s):
+    return int(s)
---
+++ level_1/KCH/20231007/짝수와홀수.py
@@ -0,0 +1,5 @@
+# 예상 시간복잡도: O(1)
+def solution(num):
+    return 'Odd' if num%2 else 'Even'
+
+#cf) ["Even", "Odd"][num & 1]
