69e1cb3a2b3abe7c31dbd6c9efc8f2cb129f979a
김채현 <kch@gimchaehyeon-ui-MacBookAir.local>
2023-11-17 10:17:27 +0900

    fix: 레벨2 - 20231114 - 김채현

--- level_2/KCH/20231114/n^2배열자르기.py
+++
@@ -1,7 +0,0 @@
-# 예상 시간복잡도: O(N**2)
-def solution(n, left, right):
-    arr = []
-    for i in range(left//n, right//n+1):
-        arr += [i+1 if i>j else j+1 for j in range(n)]
-    
-    return arr[left%n:right - left + left%n + 1] 
